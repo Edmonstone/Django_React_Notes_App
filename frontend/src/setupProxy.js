447db24e1f6d0c1af27225196ccc5174a3167bcb
Surender <surenderoffcl2002@gmail.com>
2023-07-31 00:39:01 +0530

    Simple Todo App in React with Django

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8000/api/todos/',
+      target: 'http://localhost:8000/',
       changeOrigin: true,
     })
   );
